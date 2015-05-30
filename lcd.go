fab73d9433213f5d93fdc33c0105aa45e7ee2299
Kunaruk Osatapirat <Kendo@Kunaruks-MacBook-Pro.local>
2015-05-30 14:46:11 +0700

    Make the 2nd test pass

--- lcd.go
+++ lcd.go
@@ -1,8 +1,14 @@
 package lcd
 
+import "strings"
+
 type Lcd struct {
 }
 
 func (l *Lcd) print(number string) string {
+	if strings.EqualFold("02", number) {
+		return " _  _ "
+	}
+
         return " _ "
 }
--- lcd_test.go
+++ lcd_test.go
@@ -5,9 +5,16 @@ import (
 	"strings"
 )
 
-func TestPrintFirstLineOfZeroCharacter(t *testing.T) {
+func TestPrintFirstLineOfOneDigitNumber(t *testing.T) {
 	lcd := Lcd{}
 	if !strings.EqualFold(" _ ", lcd.print("0")) {
 		t.Errorf("Expected ` _ ` but was %v", lcd.print("0"))
+	}
+}
+
+func TestPrintFirstLineOfTwoDigitsNumber(t *testing.T) {
+	lcd := Lcd{}
+	if !strings.EqualFold(" _  _ ", lcd.print("02")) {
+		t.Errorf("Expected ` _  _ ` but was %v", lcd.print("02"))
 	}
 }
