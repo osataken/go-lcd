8544171afcc1f62f382c69ef73ce9c3862a79253
Kunaruk Osatapirat <Kendo@Kunaruks-MacBook-Pro.local>
2015-05-30 14:33:08 +0700

    correct Go syntax

--- lcd_test.go
+++ lcd_test.go
@@ -1,10 +1,20 @@
 package lcd
 
-import "testing"
+import (
+	"testing"
+	"strings"
+)
 
-func printFirstLineOfZeroCharacter(t *testing.T) {
-	lcd := Lcd();
-	if " _ " != lcd.print("0") {
+type Lcd struct {
+}
+
+func (l *Lcd) print(number string) string {
+	return ""
+}
+
+func TestPrintFirstLineOfZeroCharacter(t *testing.T) {
+	lcd := Lcd{}
+	if !strings.EqualFold(" _ ", lcd.print("0")) {
 		t.Errorf("Expected ` _ ` but was %v", lcd.print("0"))
 	}
 }
